32cbe81a1a951c6a902cdae6973f5cebf55fe411
igor <ceigor94@gmail.com>
2019-03-31 08:56:37 +0200

    initial commit

---
+++ src/README.md
@@ -0,0 +1,2 @@
+# Time Ruler
+    Wep application for taking charge of your time.
---
+++ src/main/java/com/iprogrammerr/time/ruler/App.java
@@ -0,0 +1,8 @@
+package com.iprogrammerr.time.ruler;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("It works!");
+    }
+}
