e8e9549b76b9a44058db681df5b3f799d31612d7
igor <ceigor94@gmail.com>
2019-05-08 22:17:32 +0200

    Setting up profile form

--- src/main/resources/js/profile.js
+++ src/main/resources/js/profile.js
@@ -1,11 +1,20 @@
 import { router, routes, tabsNavigation } from "./app.js";
 
+const HIDDEN_DISPLAY = "none";
+const passwords = document.getElementById("passwords");
+let showPasswords = passwords.style.display == HIDDEN_DISPLAY || passwords.className == "hidden";
+
 tabsNavigation.setup(document.querySelector("div"));
 window.addEventListener("submit", e => e.preventDefault());
 document.getElementById("save").onclick = () => {
     //save
 };
-document.getElementById("resetPassword").onclick = () => {
-    //reset password
+document.getElementById("changePassword").onclick = () => {
+    if (showPasswords) {
+        passwords.style.display = "block";
+    } else {
+        passwords.style.display = HIDDEN_DISPLAY;
+    }
+    showPasswords = !showPasswords;
 };
 document.getElementById("logout").onclick = () => router.forward(routes.signOut);
