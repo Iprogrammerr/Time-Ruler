359d2bfca16569bcf44d7f6e2b885d9ee5db68f2
igor <ceigor94@gmail.com>
2019-06-01 13:19:17 +0200

    Proper activities pages links

--- src/main/java/com/iprogrammerr/time/ruler/respondent/activity/ActivitiesRespondent.java
+++ src/main/java/com/iprogrammerr/time/ruler/respondent/activity/ActivitiesRespondent.java
@@ -1,11 +1,11 @@
 package com.iprogrammerr.time.ruler.respondent.activity;
 
 import com.iprogrammerr.time.ruler.model.Identity;
-import com.iprogrammerr.time.ruler.model.param.QueryParams;
 import com.iprogrammerr.time.ruler.model.UrlQueryBuilder;
 import com.iprogrammerr.time.ruler.model.activity.ActivitiesSearch;
 import com.iprogrammerr.time.ruler.model.activity.Activity;
 import com.iprogrammerr.time.ruler.model.date.ServerClientDates;
+import com.iprogrammerr.time.ruler.model.param.QueryParams;
 import com.iprogrammerr.time.ruler.model.rendering.Page;
 import com.iprogrammerr.time.ruler.respondent.HtmlResponse;
 import com.iprogrammerr.time.ruler.view.rendering.ActivitiesViews;
@@ -66,13 +66,14 @@ public class ActivitiesRespondent {
         List<Page> pages = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
             int page = i + 1;
-            UrlQueryBuilder queryBuilder = new UrlQueryBuilder().put(QueryParams.PLAN, plan)
+            UrlQueryBuilder queryBuilder = new UrlQueryBuilder();
+            currentParams.forEach((k, v) -> v.forEach(s -> queryBuilder.put(k, s)));
+            queryBuilder.put(QueryParams.PLAN, plan)
                 .put(QueryParams.PAGE, page);
             if (pattern.length() > 0) {
                 queryBuilder.put(QueryParams.PATTERN, pattern);
             }
-            currentParams.forEach((k, v) -> v.forEach(s -> queryBuilder.put(k, s)));
-            pages.add(new Page(queryBuilder.build(ACTIVITIES), i + 1));
+            pages.add(new Page(queryBuilder.build(ACTIVITIES), page));
         }
         return pages;
     }
